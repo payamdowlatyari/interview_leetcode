d5a7cef7aadded0f3eb66f8debca00af4a77c5c4
Payam Dowlatyari <pdowlatyari@gmail.com>
2025-07-11 09:09:17 -0700

    Update test cases

--- Tree/sortedArrayToBST.js
+++ Tree/sortedArrayToBST.js
@@ -20,6 +20,21 @@ import { TreeNode } from "./data-structure/TreeNode";
  * Input: nums = [-10,-3,0,5,9]
  * Output: [0,-3,9,-10,null,5]
  * Explanation: [0,-10,5,null,-3,null,9] is also accepted:
+ * 
+ *       0
+ *      / \
+ *    -3   9
+ *    /   /
+ *  -10  5
+ * 
+ * Example 2:
+ * Input: nums = [1,3]
+ * Output: [3,1]
+ * Explanation: [1,3] and [3,1] are both a height-balanced BSTs.
+ * 
+ *       3
+ *      / \
+ *     1   null
  *
  * Time complexity: O(n)
  * Space complexity: O(log n)
--- Tree/sortedArrayToBST.test.js
+++ Tree/sortedArrayToBST.test.js
@@ -11,13 +11,19 @@ import { expect, test, describe } from "@jest/globals";
 // Explanation: [1,3] and [3,1] are both a height-balanced BSTs.
 
 describe("Describe Cases", () => {
+ 
   test("Test 1", () => {
-    const tree = sortedArrayToBST([-10, -3, 0, 5, 9]);
+    const tree = sortedArrayToBST([-10,-3,0,5,9]);
     expect(tree).toEqual(expect.any(Object));
+    expect(tree.val).toBe(0);
+    expect(tree.left.val).toBe(-10);
+    expect(tree.right.val).toBe(5);
   });
 
   test("Test 2", () => {
-    const tree = sortedArrayToBST([1, 3]);
+    const tree = sortedArrayToBST([1,3]);
     expect(tree).toEqual(expect.any(Object));
+    expect(tree.val).toBe(1);
+    expect(tree.right.val).toBe(3);
   });
 });
